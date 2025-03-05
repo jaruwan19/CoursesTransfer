7ef178a1b69c8fea62e2bf335c7543718d905666
jaruwan19 <stu6410014114@sskru.ac.th>
2025-03-05 17:31:02 +0700

    menu-officer

---
+++ app/Http/Controllers/MenuOfficerController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MenuOfficerController extends Controller
+{
+    public function selectMenu()
+    {
+        $menuItems = [
+            ['route' => '/receive_docs', 'img' => 'petition.png', 'text' => 'รับเอกสารคำร้อง'],
+            ['route' => '/receive_payment', 'img' => 'updated.png', 'text' => 'อัปเดตสถานะเพื่อแจ้งพิมพ์ใบชำระเงิน'],
+            ['route' => '/payment_update', 'img' => 'payment-service.png', 'text' => 'อัปเดตการชำระเงิน'],
+            ['route' => '/search_requests', 'img' => 'serch_request.png', 'text' => 'ค้นหาคำร้อง'],
+            ['route' => '/manage_subjects', 'img' => 'management.png', 'text' => 'จัดการรายวิชา']
+        ];
+
+        // ส่งข้อมูลเมนูไปยัง View
+        return view('officer.menu', compact('menuItems'));
+    }
+}
--- resources/views/officer/menu.blade.php
+++ resources/views/officer/menu.blade.php
@@ -1,25 +1,13 @@
 @extends('officer.layout')
+    
 @section('content')
     <div class="menuofficer-grid">
-        <a href="/receive_docs" class="menuofficer-button">
-            <img src="{{ asset('img/petition.png') }}" alt="รับเอกสารคำร้อง">
-            <span class="menuofficer-text">รับเอกสารคำร้อง</span>
-        </a>
-        <a href="/receive_payment" class="menuofficer-button">
-            <img src="{{ asset('img/updated.png') }}" alt="อัปเดตสถานะเพื่อแจ้งพิมพ์ใบชำระเงิน">
-            <span class="menuofficer-text">อัปเดตสถานะเพื่อแจ้งพิมพ์ใบชำระเงิน</span>
-        </a>
-        <a href="/payment_update" class="menuofficer-button">
-            <img src="{{ asset('img/payment-service.png') }}"alt="อัปเดตการชำระเงิน">
-            <span class="menuofficer-text">อัปเดตการชำระเงิน</span>
-        </a>
-        <a href="/search_requests" class="menuofficer-button">
-            <img src="{{ asset('img/serch_request.png') }}"alt="ค้นหาคำร้อง">
-            <span class="menuofficer-text">ค้นหาคำร้อง</span>
-        </a>
-        <a href="/manage_subjects" class="menuofficer-button">
-            <img src="{{ asset('img/management.png') }}"alt="จัดการรายวิชา">
-            <span class="menuofficer-text">จัดการรายวิชา</span>
-        </a>
+        <!-- ลูปผ่านเมนูที่ส่งมาจาก Controller -->
+        @foreach($menuItems as $menuItem)
+            <a href="{{ $menuItem['route'] }}" class="menuofficer-button">
+                <img src="{{ asset('img/' . $menuItem['img']) }}" alt="{{ $menuItem['text'] }}">
+                <span class="menuofficer-text">{{ $menuItem['text'] }}</span>
+            </a>
+        @endforeach
     </div>
 @endsection
